fbffd088f00ca359e86a5beb60bb57b12d90a5e8
xiezhuoxin <572144219@qq.com>
2018-12-15 16:17:31 +0800

    提取公共样式

---
+++ src/js/goodlist.js
@@ -0,0 +1,10 @@
+/* 
+* @Author: Marte
+* @Date:   2018-12-15 16:03:00
+* @Last Modified by:   Marte
+* @Last Modified time: 2018-12-15 16:03:10
+*/
+
+$(document).ready(function(){
+    
+});
--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,4 @@
 document.addEventListener('DOMContentLoaded',function(){
-    //管理类目：选中父类目，跳出子类目;
-    var listname=document.getElementsByClassName('cms_list_name');
-    var listchild=document.getElementsByClassName('cms_list_child');
-    var bl=document.getElementsByClassName('bl');
-    //初始化
-    listchild[0].classList.remove('hide');
-    bl[0].classList.add('blue');
-    for(var i=0;i<listname.length;i++){
-        listname[i].setAttribute('idx',i);
-        listname[i].onclick=function(){
-            for(var j=0;j<listname.length;j++){
-                listchild[j].classList.add('hide');
-            }
-            // console.log(this.getAttribute('idx'));
-            listchild[this.getAttribute('idx')].classList.remove('hide');
-        }
-    }
-
     //今日，昨日，近7天，本月
     var orbtn=document.getElementsByClassName("or_btn");
     //初始化
